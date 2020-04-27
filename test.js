2e8ba6bc201cec72d1b1223c2c7da0e7038e9ccd
Florian Varrin <florian.varrin@gmail.com>
2020-04-27 22:46:13 +0200

    Tests added

---
+++ test.js
@@ -0,0 +1,22 @@
+const Joi = require('./index.js');
+
+const schema = Joi.object({
+    id: Joi.objectId,
+    name: Joi.string(),
+});
+
+(async () => {
+    const success = await schema.validate({
+        id: '5ea7394b6b91089d144186b3',
+        name: 'John Doe',
+    });
+
+    console.log('success', success);
+
+    const failure = await schema.validate({
+        id: '1234',
+        name: 'John Doe',
+    });
+
+    console.log('failure', failure);
+})();
